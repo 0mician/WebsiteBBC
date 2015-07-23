88347d84af979ae182ada28f49f2665580a1cf72
Milt0n <cedric.lood@gmail.com>
2015-07-23 18:15:47 +0200

    finished wiring of views/model/template for the members app

--- website_bbc/members/urls.py
+++ website_bbc/members/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'scientific_societies/$', views.scientific_societies, name='scientific_society'),
+    url(r'pharmaceutical_companies/$', views.pharma_companies, name='pharmaceutical_companies'),
+    url(r'patient_associations/$', views.patient_associations, name='patient_associations'),
 ]
--- website_bbc/members/views.py
+++ website_bbc/members/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from members.models import ScientificSocietyMember
+from members.models import ScientificSocietyMember, PharmaMember, PatientAssocMember
 
 def index(request):
     context_dict = {}
@@ -11,3 +11,14 @@ def scientific_societies(request):
     members_list = ScientificSocietyMember.objects.order_by('-society')
     context_dic = {'scientific_societies' : members_list}
     return render(request, 'members/scientific_societies.html', context_dic)
+
+def patient_associations(request):
+    members_list = PatientAssocMember.objects.order_by('-society')
+    context_dic = {'patient_associations' : members_list}
+    return render(request, 'members/patient_associations.html', context_dic)
+
+def pharma_companies(request):
+    members_list = PharmaMember.objects.order_by('-society')
+    context_dic = {'pharmaceutical_companies' : members_list}
+    return render(request, 'members/pharmaceutical_companies.html', context_dic)
+
