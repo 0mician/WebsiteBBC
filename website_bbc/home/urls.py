1b437e5edd38b31a99c3e6841cccda725dd4c402
Milt0n <cedric.lood@gmail.com>
2015-07-30 20:54:25 +0200

    about pages set up in different lang, views/template wired

--- website_bbc/home/urls.py
+++ website_bbc/home/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
+    url(r'about/$', views.about, name='about'),
+    url(r'about/fr/$', views.about_fr, name='about_fr'),
+    url(r'about/nl/$', views.about_nl, name='about_nl'),
+    url(r'about/en/$', views.about_en, name='about_en'),
 ]
--- website_bbc/home/views.py
+++ website_bbc/home/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from events.models import Event
+
 def homepage(request):
-    context_dict = {}    
+    context_dict = {}
+    events = Event.objects.order_by('-start')[:5]
+    context_dict['events'] = events
     return render(request, 'home/index.html', context_dict)
 
+def about(request):
+    return render(request, 'home/about.html', {})
+
+def about_fr(request):
+    return render(request, 'home/about_fr.html', {})
+
+def about_nl(request):
+    return render(request, 'home/about_nl.html', {})
+
+def about_en(request):
+    return render(request, 'home/about_en.html', {})
