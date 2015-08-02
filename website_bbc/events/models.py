09169c7907f2d9d5976b513dc9d7052156792b56
Milt0n <cedric.lood@gmail.com>
2015-08-02 21:06:23 +0200

    added some more help text for the admin interface

--- website_bbc/events/models.py
+++ website_bbc/events/models.py
@@ -4,7 +4,8 @@ from django.template.defaultfilters import slugify
 from tinymce.models import HTMLField
 
 class SetOfFiles(models.Model):
-    name = models.CharField(max_length=225, null=False, blank=False)
+    name = models.CharField(max_length=225, null=False, blank=False,
+                            help_text="Give a meaningful name to the set of file, for example 'BBCongress2015' (max 225 characters)")
 
     def __str__(self):
         return self.name 
@@ -37,11 +38,13 @@ class Event(models.Model):
                             help_text="Give a meaningful name to the event, for example 'Belgian Brain Congress 2015' (max 255 characters)")
     slug = models.SlugField(unique=True)
     category = models.CharField(max_length=3, choices=TYPE_OF_EVENT)
-    url = models.URLField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True,
+                          help_text="optional, could be a link to the website of the event")
     start = models.DateField()
     end = models.DateField(null=True, blank=True)
     description = models.CharField(max_length=500, help_text="Give a short description of the event, it will be displayed on the homepage under events (max 500 characters)")
-    details = HTMLField(null=True)
+    details = HTMLField(null=True, 
+                        help_text="This is for the event page, you can use style here, and are free to add as much content as you like")
     uploaded_files = models.ForeignKey(SetOfFiles, null=True, blank=True)
 
     def save(self, *args, **kwargs):
