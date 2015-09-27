f1bf6973b0e0d0cb9c9ee97c8a927f7c16f0099d
Milt0n <cedric.lood@gmail.com>
2015-09-27 21:10:09 +0200

    refactoring of index to add dynamic news section

--- website_bbc/home/views.py
+++ website_bbc/home/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from events.models import Event
+from news.models import News
 
 def homepage(request):
     context_dict = {}
     events = Event.objects.order_by('-start')[:5]
     context_dict['events'] = events
+    news = News.objects.order_by('-date')[:10]
+    context_dict['news'] = news
     return render(request, 'home/index.html', context_dict)
 
 def about(request):
