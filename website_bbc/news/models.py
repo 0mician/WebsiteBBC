fb7425b484484f164ec4f064fe8120c0992dcdd3
Milt0n <cedric.lood@gmail.com>
2015-09-27 21:10:57 +0200

    news application refactoring

--- website_bbc/news/models.py
+++ website_bbc/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 from tinymce.models import HTMLField
 from events.models import SetOfFiles, File 
@@ -10,14 +11,17 @@ class News(models.Model):
     url = models.URLField(null=True, blank=True,
                           help_text="optional, could be a link to a website, more URL can be added in the details section")
     date = models.DateField()
-    description = models.CharField(max_length=500, help_text="Give a short description of the event, it will be displayed on the homepage under events (max 500 characters)")
+    description = models.CharField(max_length=500, help_text="Give a short description of the news, it will be displayed on the homepage under events (max 500 characters)")
     details = HTMLField(null=True, 
                         help_text="This is for the news details page, you can use style here, and are free to add as much content as you like")
     uploaded_files = models.ForeignKey(SetOfFiles, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(Event, self).save(*args, **kwargs)
+        super(News, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = "News"
